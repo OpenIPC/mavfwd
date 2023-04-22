15dc2ab8ae69059a5ef2aa037c11e5f39ebf90f1
whoim2 <whoim@mail.ru>
2023-04-22 19:02:01 +0300

    Add files via upload

--- mavfwd.c
+++ mavfwd.c
@@ -20,10 +20,15 @@
 
 #define MAX_MTU 9000
 
+bool verbose = false;
+
 const char *default_master = "/dev/ttyAMA0";
 const int default_baudrate = 115200;
 const char *defualt_out_addr = "127.0.0.1:14600";
 const char *default_in_addr = "127.0.0.1:14601";
+
+uint8_t ch_count = 0;
+uint16_t ch[14];
 
 struct bufferevent *serial_bev;
 struct sockaddr_in sin_out = {
@@ -39,6 +44,8 @@ static void print_usage()
 	       "  --baudrate      Serial port baudrate (%d by default)\n"
 	       "  --out           Remote output port (%s by default)\n"
 	       "  --in            Remote input port (%s by default)\n"
+	       "  --channels      RC override channels to parse after first 4 and call /root/channels.sh $ch $val, default 0\n"
+	       "  --verbose       display each packet, default not\n"	       
 	       "  --help          Display this help\n",
 	       default_master, default_baudrate, defualt_out_addr,
 	       default_in_addr);
@@ -114,14 +121,42 @@ static void signal_cb(evutil_socket_t fd, short event, void *arg)
 
 static void dump_mavlink_packet(unsigned char *data, const char *direction)
 {
-	uint8_t seq = data[2];
-	uint8_t sys_id = data[3];
-	uint8_t comp_id = data[4];
-	uint8_t msg_id = data[5];
+  uint8_t seq;
+  uint8_t sys_id;
+  uint8_t comp_id;
+  uint32_t msg_id;
 
-	printf("%s sender %d/%d\t%d\t%d\n", direction, sys_id, comp_id, seq,
-	       msg_id);
-	
+  if(data[0] == 0xFE) { //mavlink 1
+      seq = data[2];
+      sys_id = data[3];
+      comp_id = data[4];
+      msg_id = data[5];
+  } else { //mavlink 2
+      seq = data[4];
+      sys_id = data[5];
+      comp_id = data[6];
+      msg_id = data[7];
+  }
+
+	if (verbose) printf("%s %#02x sender %d/%d\t%d\t%d\n", direction, data[0], sys_id, comp_id, seq, msg_id);
+  
+  uint16_t val;
+  
+	//RC_CHANNELS ( #65 ) hook
+	if(msg_id == 65 && ch_count > 0) {
+      uint8_t offset = 18; //15 = 1ch;
+      for(uint8_t i=0; i < ch_count; i++) {
+          val = data[offset] | (data[offset+1] << 8);
+          if(ch[i] != val) {
+              ch[i] = val;
+              char buff[44];
+              sprintf(buff, "/root/channels.sh %d %d &", i+5, val);
+              system(buff);
+              if (verbose) printf("called /root/channels.sh %d %d\n", i+5, val);
+           }
+      offset = offset + 2;
+	    } //for
+	} //msg_id
 }
 
 /* https://discuss.ardupilot.org/uploads/short-url/vS0JJd3BQfN9uF4DkY7bAeb6Svd.pdf
@@ -138,10 +173,13 @@ static bool get_mavlink_packet(unsigned char *in_buffer, int buf_len,
 	if (buf_len < 6 /* header */) {
 		return false;
 	}
-	assert(in_buffer[0] == 0xFE);
+	assert(in_buffer[0] == 0xFE || in_buffer[0] == 0xFD); //mavlink 1 or 2
 
 	uint8_t msg_len = in_buffer[1];
-	*packet_len = 6 /* header */ + msg_len + 2 /* crc */;
+	if (in_buffer[0] == 0xFE)
+    *packet_len = 6 /* header */ + msg_len + 2 /* crc */; //mavlink 1
+  else
+    *packet_len = 10 /* header */ + msg_len + 2 /* crc */; //mavlink 2
 	if (buf_len < *packet_len)
 		return false;
 
@@ -154,7 +192,7 @@ static bool get_mavlink_packet(unsigned char *in_buffer, int buf_len,
 static size_t until_first_fe(unsigned char *data, size_t len)
 {
 	for (size_t i = 1; i < len; i++) {
-		if (data[i] == 0xFE) {
+		if (data[i] == 0xFE || data[i] == 0xFD) {
 			return i;
 		}
 	}
@@ -174,9 +212,9 @@ static void serial_read_cb(struct bufferevent *bev, void *arg)
 		}
 
 		// find first 0xFE and skip everything before it
-		if (*data != 0xFE) {
+		if (*data != 0xFE && *data != 0xFD) {
 			int bad_len = until_first_fe(data, in_len);
-			printf(">> Skipping %d bytes of unknown data\n",
+			if (verbose) printf(">> Skipping %d bytes of unknown data\n",
 			       bad_len);
 			evbuffer_drain(input, bad_len);
 			continue;
@@ -331,6 +369,8 @@ int main(int argc, char **argv)
 		{ "baudrate", required_argument, NULL, 'b' },
 		{ "out", required_argument, NULL, 'o' },
 		{ "in", required_argument, NULL, 'i' },
+		{ "channels", required_argument, NULL, 'c' },
+		{ "verbose", no_argument, NULL, 'v' },
 		{ "help", no_argument, NULL, 'h' },
 		{ NULL, 0, NULL, 0 }
 	};
@@ -356,6 +396,14 @@ int main(int argc, char **argv)
 			break;
 		case 'i':
 			in_addr = optarg;
+			break;
+		case 'c':
+			ch_count = atoi(optarg);
+			if(ch_count == 0) printf("rc_channels_override monitoring disabled\n");
+			else printf("rc_channels_override monitoring %d channels after first 4\n", ch_count);
+			break;
+		case 'v':
+			verbose = true;
 			break;
 		case 'h':
 		default:
